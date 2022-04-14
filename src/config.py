c9b554bdcaf52ce3de093fa6d140e536c05b37d3
leekichang <1kichang@yonsei.ac.kr>
2022-04-15 03:16:58 +0900

    spectrogram

---
+++ src/config.py
@@ -0,0 +1,8 @@
+# data_path = '../ARA08(2017_2018)/'
+# save_path = '../segmented_audio(10min)/'
+data_path = '../segmented_audio(10min)/'
+save_path = '../segmented_audio(10sec)/'
+sampling_rate = 32768
+n_segment = 60
+# n_points = sampling_rate * 60 * 10 # number of sample point for one recording(10 minutes)
+n_points = sampling_rate * 10 # number of sample point for one recording(10 sec)
